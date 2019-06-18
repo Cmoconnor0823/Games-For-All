fd879a0ea002a29a06c9b2f2801f36d632e110a9
Caitlin O'Connor <lifeisabigride@gmail.com>
2019-06-17 21:15:09 -0700

    Ruben's in class code

--- assets/javascript/videoSearch.js
+++ assets/javascript/videoSearch.js
@@ -1,18 +1,37 @@
-$(document).ready(function () {
-  console.log("ready!");
+// $(document).ready(function () {
+//   console.log("ready!");
 
-  //firebase key configuration
-  var firebaseConfig = {
-    apiKey: "AIzaSyDProkYgdjY3rTaaca5u9VipRAeaPKJnjE",
-    authDomain: "video-search-5c1ee.firebaseapp.com",
-    databaseURL: "https://video-search-5c1ee.firebaseio.com",
-    projectId: "video-search-5c1ee",
-    storageBucket: "video-search-5c1ee.appspot.com",
-    messagingSenderId: "607886212195",
-    appId: "1:607886212195:web:c661006422412d65"
-  };
-  firebase.initializeApp(firebaseConfig);
-  console.log("firebase");
+//   //firebase key configuration
+//   var firebaseConfig = {
+//     apiKey: "AIzaSyDProkYgdjY3rTaaca5u9VipRAeaPKJnjE",
+//     authDomain: "video-search-5c1ee.firebaseapp.com",
+//     databaseURL: "https://video-search-5c1ee.firebaseio.com",
+//     projectId: "video-search-5c1ee",
+//     storageBucket: "video-search-5c1ee.appspot.com",
+//     messagingSenderId: "607886212195",
+//     appId: "1:607886212195:web:c661006422412d65"
+//   };
+//   firebase.initializeApp(firebaseConfig);
+//   console.log("firebase");
 
+
+// });{
+
+$("body").on("click", "#search", function () {
+  console.log("Click")
+
+  axios.get("https://api-v3.igdb.com/games/", {
+    headers: {
+      "user-key": "aa3e0b6b7fcfadd761e44c68eee3e3e0",
+      Accept: "application/json"
+    }
+  })
+    .then(response => {
+      // Do work here
+      console.log(response.data);
+    })
+    .catch(e => {
+      console.log("error", e);
+    });
 
 });
