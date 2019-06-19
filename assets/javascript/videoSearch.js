12c3b0a12ab87ca15249d18d30dd846bee9f6287
Ruben Valdez <ruben.valdez7@gmail.com>
2019-06-18 20:26:36 -0700

    header live test

--- assets/javascript/videoSearch.js
+++ assets/javascript/videoSearch.js
@@ -1,29 +1,32 @@
-// $(document).ready(function () {
-//   console.log("ready!");
+$(document).ready(function () {
+  console.log("ready!");
 
-//   //firebase key configuration
-//   var firebaseConfig = {
-//     apiKey: "AIzaSyDProkYgdjY3rTaaca5u9VipRAeaPKJnjE",
-//     authDomain: "video-search-5c1ee.firebaseapp.com",
-//     databaseURL: "https://video-search-5c1ee.firebaseio.com",
-//     projectId: "video-search-5c1ee",
-//     storageBucket: "video-search-5c1ee.appspot.com",
-//     messagingSenderId: "607886212195",
-//     appId: "1:607886212195:web:c661006422412d65"
-//   };
-//   firebase.initializeApp(firebaseConfig);
-//   console.log("firebase");
+  //firebase key configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDProkYgdjY3rTaaca5u9VipRAeaPKJnjE",
+    authDomain: "video-search-5c1ee.firebaseapp.com",
+    databaseURL: "https://video-search-5c1ee.firebaseio.com",
+    projectId: "video-search-5c1ee",
+    storageBucket: "video-search-5c1ee.appspot.com",
+    messagingSenderId: "607886212195",
+    appId: "1:607886212195:web:c661006422412d65"
+  };
+  firebase.initializeApp(firebaseConfig);
+  console.log("firebase");
 
 
-// });{
+});
 
 $("body").on("click", "#search", function () {
   console.log("Click")
 
   axios.get("https://api-v3.igdb.com/games/", {
     headers: {
+      'Access-Control-Allow-Origin': 'https://www.igdb.com/oauth/authorize',
       "user-key": "aa3e0b6b7fcfadd761e44c68eee3e3e0",
+      
       Accept: "application/json"
+      
     }
   })
     .then(response => {
@@ -34,4 +37,11 @@ $("body").on("click", "#search", function () {
       console.log("error", e);
     });
 
+});
+
+$.ajax({
+  type: "POST",
+  beforeSend: function(request) {
+    request.setRequestHeader('Access-Control-Allow-Origin', 'https://www.igdb.com/oauth/authorize');
+  }
 });
